91ece0fe21749e6cb2517d1d7d5dc4949d46d3a6
Lukasz Terpilowski <terpilowski.l@gmail.com>
2021-11-07 23:39:19 +0100

    next one

--- tweets/views.py
+++ tweets/views.py
@@ -14,12 +14,15 @@ def home_view(request, *args,**kwargs):
 
 
 def tweet_create_view(request,*args,**kwargs):
+    print("ajax-",request.is_ajax())
     form = TweetForm(request.POST or None)
-    print(request.POST)
+    #print(request.POST)
     next_url = request.POST.get("next") or None
     if form.is_valid():
         obj = form.save(commit=False)
         obj.save()
+        if request.is_ajax():
+            return JsonResponse(obj.serialize(),status=201)
         if next_url != None and is_safe_url(next_url,allowed_urls):
             return redirect(next_url)
         form = TweetForm()
@@ -28,7 +31,7 @@ def tweet_create_view(request,*args,**kwargs):
 
 def tweet_list_view(request,*args,**kwargs):
     qs = Tweet.objects.all()
-    tweets_list = [{"id":x.id,"content":x.content,"likes":random.randint(0,500)} for x in qs]
+    tweets_list = [x.serialize() for x in qs]
     data = {
         "response":tweets_list
     }
